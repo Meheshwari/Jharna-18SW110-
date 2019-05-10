1b51a3260fd17193d75a1c146b65fb8f6ab4d563
Meheshwari <49400134+Meheshwari@users.noreply.github.com>
2019-05-10 10:55:56 +0500

    Add files via upload

---
+++ java Lab2/LabtwoTaskone.java
@@ -0,0 +1,11 @@
+class LabtwoTaskone{  
+   public static void main(String args [])
+{
+  string a,b;
+  a = args[name];
+  b = args[rollno];
+  System.out.println("name:" + a);
+  System.out.println("rollno:" + b);
+} 
+  }
+    
