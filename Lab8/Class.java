13fc657c4c82107ead7782dfe888317070648174
Meheshwari <49400134+Meheshwari@users.noreply.github.com>
2019-08-23 08:02:51 -0700

    Add files via upload

---
+++ Lab8/Class.java
@@ -0,0 +1,7 @@
+ import Task.Mycalculator;
+     class Class{
+     	public static void main(String[] args) {
+     		Mycalculator obj=new Mycalculator();
+     		System.out.print(obj.SumOfFactors(6));
+     	}
+     }
